01c3235f83040b039a71a107ce388caa062e49b3
Andreas Buchen <andreas.buchen@gmail.com>
2020-03-29 09:06:20 +0200

    Added English version of website

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.cookieconsent_options = {"message":"Diese Webseite verwendet Cookies. Wenn Sie auf der Seite weitersurfen, stimmen Sie der Cookie-Nutzung zu.","dismiss":"Verstanden","learnMore":"Mehr Informationen","link":"impressum.html#datenschutz","theme":"light-bottom"};
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-34744550-1', 'auto');
+ga('set', 'anonymizeIp', true);
+ga('send', 'pageview');
