e9950621a0a44fe7293e32213d1dfa7c16f9a2fb
Andreas Buchen <andreas.buchen@gmail.com>
2022-11-27 10:20:01 +0100

    Update analytics (2)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
 window.cookieconsent_options = {"message":"Diese Webseite verwendet Cookies. Wenn Sie auf der Seite weitersurfen, stimmen Sie der Cookie-Nutzung zu.","dismiss":"Verstanden","learnMore":"Mehr Informationen","link":"impressum.html#datenschutz","theme":"light-bottom"};
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-BE43JT9GZ0"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-BE43JT9GZ0');
-</script>
